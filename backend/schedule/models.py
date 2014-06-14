b29755d8b287d263d350ac7a513c0030db293e04
Sean Mooney <smooney@sourceallies.com>
2014-06-13 20:45:17 -0500

    Add basic data models.

--- backend/schedule/models.py
+++ backend/schedule/models.py
@@ -1,16 +1,32 @@
 from django.db import models
 
 class Event(models.Model):
-    pass
+    contacts = models.ManyToMany(Contact)
+    capacity = models.IntegerField
+    setup_start = models.DateField
+    setup_end = model.DateField
+    event_start = model.DateField
+    event_end = model.DateField
+    teardown_start = model.DateField
+    teardown_end = model.DateField
+    needed_resources = models.ManyToMany(Resource)
+    status = models.CharField(length=255, blank=False)
+    visibility = models.CharField(length=255, blank=False)
+    event_organizer = models.ForeignKey(Organization)
 
 class Location(models.Model):
-    pass
+    personel = models.ForeignKey('User')
+    location_name = models.CharField(length=255, blank=False)
+    availability = models.CharField(length=255, blank=False)
 
-class Contact(models.Model):
-    pass
+class Organization(models.Model):
+    name = models.CharField(length=255, blank=False)
+    phone_number = models.CharField(length=11, blank=True)
+    email = models.CharField(length=255)
 
 class Resource(models.Model):
-    pass
+    isFixed = models.BooleanField
+    resourceType = models.CharField(length=255, blank=False)
+    description = models.CharField(length=255, blank=True)
+    location = models.ForeignKey(Location, null=True)
 
-class Contacts(models.Model):
-    pass
