37ea680279308e4912c77f055c6d50bd97bb5af0
Sean Mooney <smooney@sourceallies.com>
2014-06-13 20:06:35 -0500

    Stub in classes for the basic models.

--- dmsc/schedule/models.py
+++ dmsc/schedule/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    pass
+
+class Location(models.Model):
+    pass
+
+class Contact(models.Model):
+    pass
+
+class Resource(models.Model):
+    pass
+
+class Contacts(models.Model):
+    pass
