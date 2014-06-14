be6dc26ec16ecfe5ee11059ddfa67cfa829c00ed
Spencer Herzberg <spencer.herzberg@gmail.com>
2014-06-14 09:33:29 -0500

    adding moar stock data for events

--- backend/dmsc/management/commands/stock_data.py
+++ backend/dmsc/management/commands/stock_data.py
@@ -15,32 +15,39 @@ class Command(BaseCommand):
         admin.set_password('admin')
         admin.save()
 
-        location, _ = Location.objects.get_or_create(
-            location_name='Location 1',
-            square_footage=100,
-            capacity=100
-        )
+        for i in range (5):
+            Location.objects.get_or_create(
+                location_name='Location {}'.format(i),
+                square_footage=100,
+                capacity=100
+            )
+
         resource = Resource.objects.get_or_create(
             is_fixed=True,
             resource_type="speakers",
             description="Bose 5.1",
-            location=location
+            location=Location.objects.get(location_name='Location 1')
         )
 
-        start = timezone.now()
-        end = start + timedelta(hours=1)
+        _1 = timezone.now()
+        _2 = timezone.now() + timedelta(hours=1)
+        _3 = timezone.now() + timedelta(hours=5)
+        _4 = timezone.now() + timedelta(hours=6)
 
-        event = Event.objects.create(
-            event_name='Testing Event',
-            setup_start=start,
-            setup_end=timezone.now(),
-            event_start=timezone.now(),
-            event_end=timezone.now(),
-            teardown_start=timezone.now(),
-            teardown_end=end,
-            status='pending',
-            visibility='public',
-            location=location,
-            contact_name='John Richard',
-            contact_email='john.richard@gmail.com'
-        )
+        dates = [_1, _2, _3, _4]
+
+        for i, date in enumerate(dates):
+            event = Event.objects.create(
+                event_name='Testing Event {}'.format(i),
+                setup_start=date,
+                setup_end=timezone.now(),
+                event_start=timezone.now(),
+                event_end=timezone.now(),
+                teardown_start=timezone.now(),
+                teardown_end=date + timedelta(hours=1),
+                status='pending',
+                visibility='public',
+                location=Location.objects.get(location_name='Location 1'),
+                contact_name='John Richard',
+                contact_email='john.richard@gmail.com'
+            )
