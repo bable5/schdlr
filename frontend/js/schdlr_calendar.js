038b2b0954ba9f27875b4da8a9d890eaabb37018
Spencer Herzberg <spencer.herzberg@gmail.com>
2014-06-14 08:29:24 -0500

    simple fullcalendar demo

--- frontend/js/schdlr_calendar.js
+++ frontend/js/schdlr_calendar.js
@@ -1,74 +1,87 @@
 $(document).ready(function() {
 
-    // page is now ready, initialize the calendar...
+    //function getAllCalendarEventsForReal(){
+    //    var schdlrAPI = "http://162.243.62.74/calendarevent";
+    //    $.getJSON( schdlrAPI, function( data ) {
+    //        var items = [];
+    //        $.each( data, function( key, val ) {
+    //            alert(key + " -- " + val);
+    //        });
+    //      });
+    //}
+    //function getDataForReal(){
+    //    var discipline = $(".discipline").val();
+    //    var location = $(".location").val();
+    //    var schdlrAPI = "http://localhost:8000/calendarevent/?location="+locaiton+"&discipline=" + discipline;
+    //    $.getJSON( schdlrAPI, function( data ) {
+    //        var items = [];
+//  //          $( "<ul/>", {
+//  //            "class": "my-new-list",
+//  //            html: items.join( "" )
+//  //          }).appendTo( "body" );
+    //      });
+    //}
 
-    var getAllCalendarEventsForReal = function(){
-        var schdlrAPI = "http://162.243.62.74/calendarevent";
-        $.getJSON( schdlrAPI, function( data ) {
-            var items = [];
-            $.each( data, function( key, val ) {
-                alert(key + " -- " + val);
-            });
-          });
-    }
-    var getDataForReal = function(){
-        var discipline = $(".discipline").val();
-        var location = $(".location").val();
-        var schdlrAPI = "http://162.243.62.74/calendarevent?location="+locaiton+"&discipline=" + discipline;
-        $.getJSON( schdlrAPI, function( data ) {
-            var items = [];
-            $.each( data, function( key, val ) {
-                alert(key + " -- " + val);
-//              items.push( "<li id='" + key + "'>" + val + "</li>" );
-            });
-            
-//            $( "<ul/>", {
-//              "class": "my-new-list",
-//              html: items.join( "" )
-//            }).appendTo( "body" );
-          });
-    }
-
-    var getData = function(){
-
-        return [{
-            events: [
-                {
-                    title: 'Event1',
-                    start: '2014-06-13'
-                },
-                {
-                    title: 'Event2',
-                    start: '2014-06-14'
-                }
-            ],
-            color: 'yellow',
-            textColor: 'black'
-        }]
-    }
+    //var getData = function(){
+    //    return [{
+    //        events: [
+    //            {
+    //                title: 'Event1',
+    //                start: '2014-06-13'
+    //            },
+    //            {
+    //                title: 'Event2',
+    //                start: '2014-06-14'
+    //            }
+    //        ],
+    //        color: 'yellow',
+    //        textColor: 'black'
+    //    }]
+    //}
     
     
     
-    $('#calendar').fullCalendar({
-        header: {
-            left: 'prev,next today',
-            center: 'title',
-            right: 'month,agendaWeek,agendaDay'
-        },
-        dayClick: function() {
-            alert('a day has been clicked!');
-        }
-    });
+    //$('#calendar').fullCalendar({
+    //    header: {
+    //        left: 'prev,next today',
+    //        center: 'title',
+    //        right: 'month,agendaWeek,agendaDay'
+    //    },
+    //    dayClick: function() {
+    //        alert('a day has been clicked!');
+    //    }
+    //});
     
 
-    var x = getData() ;
-   $('#calendar').fullCalendar( 'renderEvent', x);
+   // var x = getData() ;
+   //$('#calendar').fullCalendar( 'renderEvent', x);
 
 
 
-    $(".filter-btn").on("click", function(){
-       getAllCalendarEventsForReal(); 
-    });
-    
+    //$(".filter-btn").on("click", function(){
+    //   getAllCalendarEventsForReal();
+    //});
+
+    $(document).ready(function() {
+		$('#calendar').fullCalendar({
+			header: {
+				left: 'prev,next today',
+				center: 'title',
+				right: 'month,agendaWeek,agendaDay'
+			},
+			defaultDate: '2014-06-14',
+			editable: true,
+			events: {
+				url: 'http://localhost:8000/calendarevent/',
+				error: function() {
+					$('#script-warning').show();
+				}
+			},
+			loading: function(bool) {
+				$('#loading').toggle(bool);
+			}
+		});
+
+	});
 
 });
